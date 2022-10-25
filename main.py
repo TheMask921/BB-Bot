df029458a627ca5865b221562ba218beff7ed054
TheMask921 <stonfras03@gmail.com>
2022-10-25 13:13:30 +0100

    api changes, need to work on OctoRest

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import discord, json
-from octorest import OctoRest
-from aiohttp import request
+import discord, requests, json
 from discord.ext import commands
 
 
 with open("token.json") as token:
     token = json.load(token)
 
-with open("apikey.json") as apikey:
-    apikey = json.load(apikey)
-
 with open("url.json") as url:
     url = json.load(url)
 
+with open("apikey.json") as apikey:
+    url = json.load(apikey)
 
 intents = discord.Intents.default()
 
@@ -20,25 +17,16 @@ bot = commands.Bot(command_prefix="?")
 
 @bot.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(bot))
+    print ("We have logged in as {0.user}".format(bot))
 
-def make_client(url, apikey):
-    try: 
-        client = OctoRest(url = url, apikey = apikey)
-        return client
-    except ConnectionError as ex:
-        print(ex)
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.channel.send("pong")
-
-@bot.command(name = "files")
-async def file_names(client):
-     message = "The GCODE files currently on the printer are:\n\n"
-     for k in client.files()['files']:
-         message += k['name'] + "\n"
-     print(message) 
-    
+@bot.command(name = "dog")
+async def send(ctx):
+    r = requests.get("https://dog.ceo/api/breeds/image/random")
+    data = r.json()
+    if r.status_code == 200:
+        print(f"API returned a {r.status_code} status.")
+        await ctx.channel.send(data["message"])
+    else:
+        print(f"API returned a {r.status_code} status.")
 
 bot.run(token)
